fc474a95605f221a889dce9ada08cb70f8873915
Zethax <michele.grosso90@gmail.com>
2018-09-17 13:50:50 +0200

    Create DetailScreen.js

---
+++ DetailScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, StatusBar,} from 'react-native';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import { SearchBar } from 'react-native-elements'
+
+export default class DetailScreen extends React.Component{
+
+    render(){
+        return(
+            <View>
+            <Text style={{fontSize:50}}> Test </Text>
+
+            </View>
+        )
+    }
+}
