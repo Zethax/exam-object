18d7a940cdc741a34f8820c16768185bc69e0ac5
Zethax <michele.grosso90@gmail.com>
2018-09-17 21:41:51 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, StatusBar} from 'react-native';
+import {StyleSheet, StatusBar,View} from 'react-native';
 import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 import { SearchBar } from 'react-native-elements'
 
@@ -11,24 +11,23 @@ import DetailScreen from './component/DetailScreen'
 StatusBar.setHidden(true);
 
 //add stack navigator
-const Navigazione= createStackNavigator({
+const NavigazioneLista= createStackNavigator({
   Lista:{ //nome da richiamare nella navigazione, ps pagina main quindi value anche in roostack
     screen: ListaScreen,  //nome da dare come titolo alla classe/componente
-    navigationOption:{
-      title:'Menù'
-    }
+    
   },
   Details:{
     screen: DetailScreen
   },
- 
+  
   //add new stack navigator
-})
+},
+)
 
 //tab navigation
 const RootStack= createBottomTabNavigator({
   Lista:{
-    screen:ListaScreen
+    screen:NavigazioneLista//gli importo LO STACK navigator che a che fare E NON LA CLASSE
   },
   Carrello:{
     screen: CarrelloScreen
@@ -37,6 +36,9 @@ const RootStack= createBottomTabNavigator({
 
 export default class App extends React.Component{
   render(){
-    return( <RootStack />)
+    return( 
+              <RootStack />
+            )
   }
 }
+
